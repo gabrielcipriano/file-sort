8d715e4443a6106cfb43e84ee4e827886bcab156
Jeiel <jeiel.moura.silva@gmail.com>
2023-11-02 17:57:57 -0300

    🚧 circular chunck test

--- core/circular-chunck_test.go
+++ core/circular-chunck_test.go
@@ -1,26 +1,49 @@
+package core_test
+
+import (
+	"errors"
+
+	core "github.com/gabrielcipriano/file-sort/core"
+)
+
+var (
+	errCantPush = errors.New("Can't Push")
+	errCantPop  = errors.New("Can't Pop")
+)
+
+func addElements(cc *core.CircularChunck, n int) (int, error) {
+	for i := 0; i < n; i++ {
+		if !cc.Push(i) {
+			return i, errCantPush
+		}
+	}
+	return n, nil
+}
+
+func removeElements(cc *core.CircularChunck, n int) ([]int, error) {
+	answer := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		elem, ok := cc.Pop()
+		if !ok {
+			return answer, errCantPop
+		}
+		answer = append(answer, elem.(int))
+	}
+	return answer, nil
+}
+
 // func main() {
 // 	size := 10
 // 	cc := core.NewCircularChunck(size)
 
-// 	for i := 0; i < size; i++ {
-// 		err := cc.Push(i)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 	}
-
-// 	for i := 0; i < size; i++ {
-// 		val, err := cc.Pop()
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		fmt.Println(val)
-// 	}
-
-// 	val, err := cc.Pop()
+// 	_, err := addElements(cc, size)
 // 	if err != nil {
 // 		panic(err)
 // 	}
-// 	fmt.Println(val)
+
+// 	_, err := removeElements(cc, size)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
 // }
